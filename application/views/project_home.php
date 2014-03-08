95b2c51496a7894a29f51aafe2a2336b52f16a53
alim <alim.gokkaya@gmail.com>
2014-03-08 14:33:47 +0200

    asd

--- application/views/project_home.php
+++ application/views/project_home.php
@@ -82,8 +82,8 @@
                 <h2>Welcome</h2>
 		<p>This is the home page of our term project.</p>
                 <h2>Project Proposal</h2>
-                <p>You can access project proposeal <a href="proposal.pdf">here</a></p>
-                <p><a href="er_diagram.svg">ER diagram</a></p>
+                <p>You can access project proposeal <a href="res/proposal.pdf">here</a></p>
+                <p><a href="res/er_diagram.svg">ER diagram</a></p>
 	</div>
 
 	<p class="footer">CS353 Group 8 - 2014 Bilkent University</p>
